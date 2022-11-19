0612f40ed56f2506592b6d677d83856062997ed5
RKCodes12 <108786733+RKCodes12@users.noreply.github.com>
2022-11-19 16:14:55 +0530

    First Commit....

---
+++ src/Lead/Test1.java
@@ -0,0 +1,17 @@
+package Lead;
+
+public class Test1 {
+	
+    public static void main(String [] args) {
+    	
+    	String hungry ="yes";
+    	
+    	if(hungry == "yes") {
+    		System.out.println("eat somting");
+    	}else {
+			System.out.println("Do Work");
+		}
+		
+	}
+
+}
